9380e09a7e75c84dded2ea85b0d734f81a09c0e7
Udit Karode <udit.karode@gmail.com>
2022-04-23 01:16:24 +0530

    okay this installs and works

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,59 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import {
   configureFonts,
   DefaultTheme,
   Provider as PaperProvider,
-} from 'react-native-paper';
-import {StatusBar, StatusBarStyle} from 'expo-status-bar';
-import {SafeAreaView} from 'react-native-safe-area-context';
-import {Appearance, View} from 'react-native';
-import {ThemeProvider, darkTheme, lightTheme} from './utils/ThemeContext';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import changeNavigationBarColor from 'react-native-navigation-bar-color';
-import Home from './screens/Home';
-import {StyleSheet} from 'react-native';
+} from "react-native-paper";
+import { StatusBar, StatusBarStyle } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Appearance, View } from "react-native";
+import { ThemeProvider, darkTheme, lightTheme } from "./utils/ThemeContext";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import changeNavigationBarColor from "react-native-navigation-bar-color";
+import Home from "./screens/Home";
+import { StyleSheet } from "react-native";
 
 const theme = {
   ...DefaultTheme,
   fonts: configureFonts({
     android: {
       regular: {
-        fontFamily: 'Inter-Regular',
-        fontWeight: 'normal',
+        fontFamily: "Inter-Regular",
+        fontWeight: "normal",
       },
       medium: {
-        fontFamily: 'Inter-SemiBold',
-        fontWeight: 'normal',
+        fontFamily: "Inter-SemiBold",
+        fontWeight: "normal",
       },
       light: {
-        fontFamily: 'Inter-Light',
-        fontWeight: 'normal',
+        fontFamily: "Inter-Light",
+        fontWeight: "normal",
       },
       thin: {
-        fontFamily: 'Inter-Thin',
-        fontWeight: 'normal',
+        fontFamily: "Inter-Thin",
+        fontWeight: "normal",
       },
     },
   }),
   roundness: 6,
   colors: {
     ...DefaultTheme.colors,
-    primary: '#FFE082',
-    accent: '#f1c40f',
+    primary: "#FFE082",
+    accent: "#f1c40f",
   },
 };
 
 export default function Main() {
   // keep track of app theme so that it can be changed internally
   const [currentTheme, setCurrentTheme] = useState(() =>
-    Appearance.getColorScheme() === 'dark' ? darkTheme : lightTheme,
+    Appearance.getColorScheme() === "dark" ? darkTheme : lightTheme
   );
 
   // change app theme when the system theme changes
   useEffect(() => {
     const listener = (prefs: Appearance.AppearancePreferences) => {
-      setCurrentTheme(prefs.colorScheme === 'dark' ? darkTheme : lightTheme);
+      setCurrentTheme(prefs.colorScheme === "dark" ? darkTheme : lightTheme);
     };
 
     Appearance.addChangeListener(listener);
@@ -68,7 +68,7 @@ export default function Main() {
     changeNavigationBarColor(
       currentTheme.bg,
       currentTheme.bg === lightTheme.bg,
-      false,
+      false
     );
   }, [currentTheme]);
 
@@ -90,7 +90,8 @@ export default function Main() {
             theme: currentTheme,
             switchTheme: () =>
               setCurrentTheme(currentTheme.isDark ? lightTheme : darkTheme),
-          }}>
+          }}
+        >
           <View style={ls.root}>
             <SafeAreaView style={ls.safeArea}>
               <App />
@@ -107,7 +108,7 @@ const Stack = createStackNavigator<StackParamList>();
 
 function App() {
   return (
-    <Stack.Navigator screenOptions={{headerShown: false}}>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Home" component={Home} />
     </Stack.Navigator>
   );
