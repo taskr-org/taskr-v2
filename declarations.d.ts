b87af0efe301e1155fd6222955036dd33473bdbd
Udit Karode <udit.karode@gmail.com>
2022-04-23 01:33:45 +0530

    chore: integrate svg-transformer

--- declarations.d.ts
+++ declarations.d.ts
@@ -10,3 +10,10 @@ type AppTheme = {
 type StackParamList = {
   Home: undefined;
 };
+
+declare module "*.svg" {
+  import React from "react";
+  import { SvgProps } from "react-native-svg";
+  const content: React.FC<SvgProps>;
+  export default content;
+}
